13652f3034f6a0bfa27499f61a97ffc3c99b132d
Timo Waali <waali.timo@gmail.com>
2026-03-05 10:43:41 +0200

    1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -6,6 +6,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // Laskee kaikki palautteet yhteen, keskiarvon ja positiivisten palautteiden prosenttiosuuden
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  console.log(average)
+  const positive = (good / all * 100)
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -13,9 +19,12 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
+      <div>good: {good}</div>
+      <div>neutral: {neutral}</div>
+      <div>bad: {bad}</div>
+      <div>all: {all}</div>
+      <div>average: {average}</div>
+      <div>positive: {positive} %</div>
     </div>
   )
 }
