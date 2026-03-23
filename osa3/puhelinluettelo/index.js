5fa072e286de6fdfca42eeadacbe46e3aeadbe9a
Timo Waali <waali.timo@gmail.com>
2026-03-23 10:58:43 +0200

    3.4

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Kovakoodattu data
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -48,6 +48,12 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
