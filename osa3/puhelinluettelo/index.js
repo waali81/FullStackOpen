8b50340234a02503dad7ec5f7fb03b4982992f50
Timo Waali <waali.timo@gmail.com>
2026-03-23 12:04:29 +0200

    3.6

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -61,6 +61,16 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({error: 'name or number missing'})
+  }
+
+  const nameExists = persons.some(p => p.name === body.name)
+
+  if (nameExists) {
+    return response.status(400).json({error: 'name must be unique'})
+  }
+
   const person = {
     id: Math.floor(Math.random() * 10000),
     name: body.name,
