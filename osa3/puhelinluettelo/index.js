13812bab56d4a573467baa94e677ea6659bcf55c
Timo Waali <waali.timo@gmail.com>
2026-03-24 09:44:55 +0200

    3.8

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -3,9 +3,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
 
-// Kovakoodattu data
+morgan.token('body', (request) => {
+  return request.method === 'POST' ? JSON.stringify(request.body) : ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 let persons = [
   {
     id: 1,
