469016cc3544b4ef40e96e9b21cb584eee377b76
Timo Waali <waali.timo@gmail.com>
2026-03-31 17:22:22 +0300

    3.14

--- osa3/puhluetteloback/index.js
+++ osa3/puhluetteloback/index.js
@@ -15,29 +15,6 @@ morgan.token('body', (request) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-/* let persons = [
-   {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-] */
-
 app.get('/info', (request, response) => {
   Person.countDocuments({}).then(count => {
     const date = new Date()
@@ -55,17 +32,6 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-/* app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-}) */
-
 app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id)
     .then(person => {
@@ -80,12 +46,6 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-/* app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-  response.status(204).end()
-}) */
-
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id)
     .then(() => {
@@ -95,30 +55,6 @@ app.delete('/api/persons/:id', (request, response) => {
       response.status(400).send({ error: 'malformatted id' })
     })
 })
-
-/* app.post('/api/persons', (request, response) => {
-  const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({error: 'name or number missing'})
-  }
-
-  const nameExists = persons.some(p => p.name === body.name)
-
-  if (nameExists) {
-    return response.status(400).json({error: 'name must be unique'})
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 10000),
-    name: body.name,
-    number: body.number
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
-}) */
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
