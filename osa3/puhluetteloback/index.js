57af6988a88d459f173469f6d0176061c6be4eb4
Timo Waali <waali.timo@gmail.com>
2026-04-01 09:54:36 +0300

    testiä ja 3.17

--- osa3/puhluetteloback/index.js
+++ osa3/puhluetteloback/index.js
@@ -70,6 +70,26 @@ app.post('/api/persons', (request, response, next) => {
   .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save()
+    })
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
