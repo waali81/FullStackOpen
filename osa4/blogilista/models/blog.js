a6f6708bd07e60d50f9211753ed5f1edf8e30dca
Timo Waali <waali.timo@gmail.com>
2026-04-09 14:43:57 +0300

    4.2

---
+++ osa4/blogilista/models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  author: String,
+  url: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+  },
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
