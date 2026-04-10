6062273ba493c5b3589f4728575b9e0fd8e57b89
Timo Waali <waali.timo@gmail.com>
2026-04-10 10:42:19 +0300

    4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,4 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite
+  })
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
