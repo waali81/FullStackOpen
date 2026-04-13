ae0f724d624648dc599a65ce0a052431f95590f1
Timo Waali <waali.timo@gmail.com>
2026-04-13 12:20:06 +0300

    4.8

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
+blogsRouter.get('/', async (request, response) => {
   Blog.find({}).then(blogs => {
     response.json(blogs)
   })
---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,53 @@
+const { test, describe, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+// Testidata
+const initialBlogs = [
+  {
+    title: 'First blog',
+    author: 'Alice',
+    url: 'http://example.com/1',
+    likes: 5,
+  },
+  {
+    title: 'Second blog',
+    author: 'Bob',
+    url: 'http://example.com/2',
+    likes: 3,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+
+  await Promise.all(promiseArray)
+})
+
+describe('when there are blogs in db', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
