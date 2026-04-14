5199e41d3661f63b02b1f05252f44f61f08bda9e
Timo Waali <waali.timo@gmail.com>
2026-04-14 09:21:22 +0300

    4.9

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -5,29 +5,11 @@ const supertest = require('supertest')
 const helper = require('./test_helper.js')
 const app = require('../app')
 const Blog = require('../models/blog')
-
 const api = supertest(app)
-
-
-/* const initialBlogs = [
-  {
-    title: 'First blog',
-    author: 'Alice',
-    url: 'http://example.com/1',
-    likes: 5,
-  },
-  {
-    title: 'Second blog',
-    author: 'Bob',
-    url: 'http://example.com/2',
-    likes: 3,
-  },
-] */
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  /* const blogObjects = initialBlogs.map(blog => new Blog(blog)) */
   const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
   const promiseArray = blogObjects.map(blog => blog.save())
 
@@ -45,9 +27,18 @@ describe('when there are blogs in db', () => {
   test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
 
-    /* assert.strictEqual(response.body.length, initialBlogs.length) */
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
+
+  test('blog posts have id property instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+
+    const blog = response.body[0]
+
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  })
+
 })
 
 after(async () => {
