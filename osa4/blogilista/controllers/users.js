49a9ad9972a0a3f860236d99fcf0ce6d1de3c229
Timo Waali <waali.timo@gmail.com>
2026-04-20 10:52:06 +0300

    4.16

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -7,20 +7,32 @@ usersRouter.get('/', async (request, response) => {
   response.json(users)
 })
 
-usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+usersRouter.post('/', async (request, response, next) => {
+  try {
+    const { username, name, password } = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+    // password validointi
+    if (!password || password.length < 3) {
+      return response.status(400).json({
+        error: 'password must be at least 3 characters long'
+      })
+    }
 
-  const user = new User({
-    username,
-    name,
-    passwordHash
-  })
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const savedUser = await user.save()
-  response.status(201).json(savedUser)
+    const user = new User({
+      username,
+      name,
+      passwordHash
+    })
+
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = usersRouter
--- osa4/blogilista/tests/test_helper.js
+++ osa4/blogilista/tests/test_helper.js
@@ -1,5 +1,6 @@
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -21,7 +22,13 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
   initialBlogs,
   blogsInDb,
+  usersInDb,
 }
