8786e06d55edcda6d0608cb6d5a400d8ce65e48c
Timo Waali <waali.timo@gmail.com>
2026-05-06 11:07:57 +0300

    5.7

--- Osa5/bloglist-frontend/src/components/Blog.jsx
+++ Osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,49 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {/* suppea näkymä */}
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+
+      {/* kokonäkymä */}
+      <div style={showWhenVisible}>
+        <div>
+          {blog.title} {blog.author}{" "}
+          <button onClick={toggleVisibility}>hide</button>
+        </div>
+
+        <div>{blog.url}</div>
+
+        <div>
+          likes {blog.likes}{" "}
+          <button>like</button>
+        </div>
+
+        <div>{blog.user?.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
