d9dc98d1a889cf6f530f71fb170aa8103f3f6672
Timo Waali <waali.timo@gmail.com>
2026-05-07 09:35:06 +0300

    5.10

--- Osa5/bloglist-frontend/src/App.jsx
+++ Osa5/bloglist-frontend/src/App.jsx
@@ -161,9 +161,16 @@ const App = () => {
         <BlogForm createBlog={handleCreateBlog} />
       </Togglable>
 
-      {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-      ))}
+      {[...blogs]
+        .sort((blog1, blog2) => blog2.likes - blog1.likes)
+        .map(blog => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleLike={handleLike}
+          />
+        ))
+      }
     </div>
   )
 }
