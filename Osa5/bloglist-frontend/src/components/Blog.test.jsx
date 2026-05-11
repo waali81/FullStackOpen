d9fd5ef04d9de1ff9a1064b950429f37379f7645
Timo Waali <waali.timo@gmail.com>
2026-05-11 10:14:14 +0300

    5.14

--- Osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ Osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author, but not url or likes by default', () => {
@@ -33,4 +34,32 @@ test('renders title and author, but not url or likes by default', () => {
   expect(
     screen.queryByText('likes 7')
   ).not.toBeVisible()
+})
+
+test('shows url, likes and user when view button is clicked', async () => {
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    user: {
+      username: 'twaali',
+      name: 'Timo Waali'
+    }
+  }
+
+  const user = {
+    username: 'twaali'
+  }
+
+  render(<Blog blog={blog} user={user} />)
+
+  const button = screen.getByText('view')
+
+  const userEventSetup = userEvent.setup()
+  await userEventSetup.click(button)
+
+  screen.getByText('https://reactpatterns.com/')
+  screen.getByText('likes 7')
+  screen.getByText('Timo Waali')
 })
