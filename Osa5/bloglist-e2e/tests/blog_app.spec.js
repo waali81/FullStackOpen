d3ba983217477537d17ddec92c1c2c3cd7556a8e
Timo Waali <waali.timo@gmail.com>
2026-05-13 12:32:09 +0300

    iso kirjain

--- Osa5/bloglist-e2e/tests/blog_app.spec.js
+++ Osa5/bloglist-e2e/tests/blog_app.spec.js
@@ -1,35 +1,67 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page, request }) => {
-    await request.post('http://localhost:3003/api/testing/reset')
+    beforeEach(async ({ page, request }) => {
+        await request.post('http://localhost:3003/api/testing/reset')
 
-    await request.post('http://localhost:3003/api/users', {
-      data: {
-        name: 'Teppo Testaaja',
-        username: 'Testi',
-        password: 'salasana'
-      }
+        await request.post('http://localhost:3003/api/users', {
+        data: {
+            name: 'Teppo Testaaja',
+            username: 'Testi',
+            password: 'salasana'
+        }
+        })
+
+        await page.goto('http://localhost:5173')
     })
 
-    await page.goto('http://localhost:5173')
-  })
+    test('Login form is shown', async ({ page }) => {
+        await expect(
+            page.getByText('Log in to application')
+        ).toBeVisible()
 
-  test('Login form is shown', async ({ page }) => {
-    await expect(
-      page.getByText('log in to application')
-    ).toBeVisible()
+        await expect(
+            page.getByLabel('username')
+        ).toBeVisible()
 
-    await expect(
-      page.getByLabel('username')
-    ).toBeVisible()
+        await expect(
+            page.getByLabel('password')
+        ).toBeVisible()
 
-    await expect(
-      page.getByLabel('password')
-    ).toBeVisible()
+        await expect(
+            page.getByRole('button', { name: 'login' })
+        ).toBeVisible()
+    })
 
-    await expect(
-      page.getByRole('button', { name: 'login' })
-    ).toBeVisible()
-  })
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByLabel('username').fill('Testi')
+
+            await page.getByLabel('password').fill('salasana')
+
+            await page.getByRole('button', { name: 'login' }).click()
+
+            await expect(
+                page.getByText('Teppo Testaaja logged in')
+            ).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByLabel('username').fill('wrong')
+
+            await page.getByLabel('password').fill('wrong')
+
+            await page.getByRole('button', { name: 'login' }).click()
+
+            const errorDiv = page.locator('.error')
+
+            await expect(errorDiv).toContainText(
+                'wrong username/password'
+            )
+
+            await expect(
+            page.getByText('Teppo Testaaja logged in')
+            ).not.toBeVisible()
+        })
+    })
 })
