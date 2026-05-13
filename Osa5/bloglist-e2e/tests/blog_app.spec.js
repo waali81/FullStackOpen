fbaf094defb4d9a2939f25e463ffc6d30906cf52
Timo Waali <waali.timo@gmail.com>
2026-05-13 13:22:06 +0300

    5.19

--- Osa5/bloglist-e2e/tests/blog_app.spec.js
+++ Osa5/bloglist-e2e/tests/blog_app.spec.js
@@ -36,9 +36,7 @@ describe('Blog app', () => {
     describe('Login', () => {
         test('succeeds with correct credentials', async ({ page }) => {
             await page.getByLabel('username').fill('Testi')
-
             await page.getByLabel('password').fill('salasana')
-
             await page.getByRole('button', { name: 'login' }).click()
 
             await expect(
@@ -48,9 +46,7 @@ describe('Blog app', () => {
 
         test('fails with wrong credentials', async ({ page }) => {
             await page.getByLabel('username').fill('wrong')
-
             await page.getByLabel('password').fill('wrong')
-
             await page.getByRole('button', { name: 'login' }).click()
 
             const errorDiv = page.locator('.error')
@@ -62,6 +58,26 @@ describe('Blog app', () => {
             await expect(
             page.getByText('Teppo Testaaja logged in')
             ).not.toBeVisible()
+        })
+    })
+
+    describe('When logged in', () => {
+        beforeEach(async ({ page }) => {
+            await page.getByLabel('username').fill('Testi')
+            await page.getByLabel('password').fill('salasana')
+            await page.getByRole('button', { name: 'login' }).click()
+        })
+
+        test('a new blog can be created', async ({ page }) => {
+            await page.getByRole('button', { name: 'new blog' }).click()
+            await page.getByLabel('title').fill('Playwright blog')
+            await page.getByLabel('author').fill('Teppo Testaaja')
+            await page.getByLabel('url').fill('www.testi.fi')
+            await page.getByRole('button', { name: 'create' }).click()
+
+            const blogElement = page.getByText('Playwright blog Teppo Testaaja')
+
+            await expect(blogElement.first()).toBeVisible()
         })
     })
 })
