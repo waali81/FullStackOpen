043ba26f045b992ddb2626e819c721bcd80aef6f
Timo Waali <waali.timo@gmail.com>
2026-05-21 11:27:04 +0300

    6.2

---
+++ Osa6/zustand-anecdotes/src/App.jsx
@@ -0,0 +1,32 @@
+
+import { useAnecdotes, useAnecdoteActions } from './store'
+
+const App = () => {
+  const anecdotes = useAnecdotes()
+
+  const { vote } = useAnecdoteActions()
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
