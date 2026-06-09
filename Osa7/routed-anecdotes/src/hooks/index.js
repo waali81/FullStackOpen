8dc4eefbc86912a70c11f6b4905532d12f032baf
Timo Waali <waali.timo@gmail.com>
2026-06-09 10:38:50 +0300

    7.4

--- Osa7/routed-anecdotes/src/hooks/index.js
+++ Osa7/routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import anecdoteService from '../services/anecdotes'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
@@ -17,4 +18,16 @@ export const useField = (type) => {
     onChange,
     reset
   }
+}
+
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+
+  useEffect(() => {
+    anecdoteService.getAll().then(data => {
+      setAnecdotes(data)
+    })
+  }, [])
+
+  return { anecdotes }
 }
