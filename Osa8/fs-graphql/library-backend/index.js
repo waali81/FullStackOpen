daed7e99a018b48bf81765136457297c966c3733
Timo Waali <waali.timo@gmail.com>
2026-08-06 10:42:55 +0300

    8.5

--- Osa8/fs-graphql/library-backend/index.js
+++ Osa8/fs-graphql/library-backend/index.js
@@ -114,7 +114,7 @@ const typeDefs = /* GraphQL */ `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -124,10 +124,21 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books
+      let filteredBooks = books
+
+      if (args.author) {
+        filteredBooks = filteredBooks.filter(
+          book => book.author === args.author
+        )
       }
-      return books.filter(book => book.author === args.author)
+
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter(
+          book => book.genres.includes(args.genre)
+        )
+      }
+
+      return filteredBooks
     },
     allAuthors: () => authors,
   },
