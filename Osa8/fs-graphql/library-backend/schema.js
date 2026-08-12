77041615db2978ea61431bafa025a206ffeff125
Timo Waali <waali.timo@gmail.com>
2026-08-12 09:22:54 +0300

    8.13 refaktorointi

---
+++ Osa8/fs-graphql/library-backend/schema.js
@@ -0,0 +1,38 @@
+const typeDefs = /* GraphQL */ `
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+    id: ID!
+  }
+  
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
+  
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+`
+module.exports = typeDefs
